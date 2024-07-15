0cfef05725756f399248cab8fe3524f99540ac1c
Fakhri Mammedzade <safm11060917@gmail.com>
2024-07-15 21:11:01 +0400

    add state

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,8 +1,16 @@
 import "./ProductCard.css";
-import { LuPlus } from "react-icons/lu";
+import { useState } from "react";
+import { LuCheck, LuHeart, LuPlus } from "react-icons/lu";
 function ProductCard({productPrice , productTitle , img}) {
+    const [selected, setSelected] = useState(false);
+    const [liked, setLiked] = useState(false);
   return (
     <div className="product-card">
+        <div className="product-like">
+            <button className={liked && "liked"} onClick={() => setLiked(!liked)}>
+                <LuHeart color={liked ? "#FF8585" : "EAEAEA"} fill={liked ? "#FF8585" : "none"}  size={16}/>
+            </button>
+        </div>
         <div className="product-card__image">
             <img src={img} alt=""/>
         </div>
@@ -14,7 +22,7 @@ function ProductCard({productPrice , productTitle , img}) {
                 <p>Цена:</p>
                 <h5>{productPrice}</h5>
             </div>
-            <button><LuPlus size={11}/></button>
+            <button className={selected && "selected"} onClick={() => setSelected(!selected)}>{!selected ? (<LuPlus size={12}/>) : (<LuCheck size={12} color="white" />)}</button>
         </div>
     </div>
   )
